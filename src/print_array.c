9a38dfcdd77a8054011830b8b2c022971a9197a2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-28 16:01:02 -0300

    perf: reduce function
    The function now runs through the array just once and print its
    arguments to the terminal. Previously, it ran through the array twice.
    Once to find its size, and once more to print the values.

--- src/print_array.c
+++ src/print_array.c
@@ -2,21 +2,15 @@
 
 #include <print_array.h>
 
-int get_size(char *array[]) {
-	int index = 0;
-	while (array[index] != NULL) index++;
-
-	return index;
-}
-
 void print_array(char *array[], char *name) {
-	int size = get_size(array);
-
 	if (name != NULL)
 		printf("%s: ", name);
 	printf("{");
 
-	for (int index = 0; index < size - 1; index++)
+	int index = 0;
+	while(array[index + 1] != NULL) {
 		printf("\"%s\", ", array[index]);
-	printf("\"%s\"}\n", array[size - 1]);
+		index++;
+	}
+	printf("\"%s\"}\n", array[index]);
 }
