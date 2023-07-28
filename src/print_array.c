1fe3f6ba42b583a2c3c1106756e03011123a8b29
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-28 15:00:04 -0300

    refactor: update print_array function
    The print_array function doesn't require the array size anymore.
    Furthermore, it also accepts NULL as the name argument. By doing so, it
    doesn't print the array name.

--- src/print_array.c
+++ src/print_array.c
@@ -2,8 +2,20 @@
 
 #include <print_array.h>
 
-void print_array(char *array[], char *name, int size) {
-	printf("%s: {", name);
+int get_size(char *array[]) {
+	int index = 0;
+	while (array[index] != NULL) index++;
+
+	return index;
+}
+
+void print_array(char *array[], char *name) {
+	int size = get_size(array);
+
+	if (name != NULL)
+		printf("%s: ", name);
+	printf("{");
+
 	for (int index = 0; index < size - 1; index++)
 		printf("\"%s\", ", array[index]);
 	printf("\"%s\"}\n", array[size - 1]);
